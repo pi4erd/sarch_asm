6d08723c9922c43431b7a9c25fe8361a9113f883
pi4erd <piuuttri@gmail.com>
2023-12-13 19:36:40 +0100

    Deleted debug remnants Modified installation script

--- install.sh
+++ install.sh
@@ -3,9 +3,11 @@
 # An install script for Sarch32
 # Compiles and installs in /usr/local/bin
 
+INSTALL_DIR=/usr/local/bin
+
 cargo build --release
 
-echo "Trying to install in /usr/local/bin"
+echo "Trying to install in $INSTALL_DIR"
 echo "Requesting permission..."
-sudo cp ./target/release/sarch_asm /usr/local/bin/
+sudo cp ./target/release/sarch_asm $INSTALL_DIR
 
