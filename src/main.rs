5bc47baebc8224eaa96ef50f06496e827efbf541
pi4erd <piuuttri@gmail.com>
2023-12-13 22:47:59 +0100

    Started using crate version from env Synced current crate version

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use crate::{objgen::ObjectFormat, linker::Linker};
 
 use std::{fs, env::args, process::ExitCode};
 
-const VERSION: &'static str = "v0.1.4-1";
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 const GITHUB: &'static str = "https://github.com/pi4erd/sarch_asm";
 
 fn print_version() {
